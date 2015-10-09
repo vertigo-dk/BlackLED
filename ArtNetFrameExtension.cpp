50657fd90cdb2995532a66086d4207b040b47418
Varde <varde@Varde-Mac-mini.local>
2015-10-09 17:43:00 +0200

    ArtNetExtension (not yet working, but not in use)

---
+++ ArtNetFrameExtension.cpp
@@ -0,0 +1,96 @@
+
+#include <stdio.h>
+#include <stdlib.h> // malloc
+#include <string.h> // memcpy
+#include "ArtNetFrameExtension.h"
+
+
+ArtNodeExtended::ArtNodeExtended() {
+}
+
+ArtNodeExtended::ArtNodeExtended(ArtConfig & config, int size, unsigned char * buffer) {
+    this->config = &config;
+    this->bufferSize = size;
+    this->buffer = buffer;
+}
+
+int ArtNodeExtended::sizeOfExtendedPollReply(){
+  return sizeof(T_Ext_ArtPollReply) + sizeof(short)*config->numPorts;
+}
+
+void ArtNodeExtended::createExtendedPollReply(){
+  T_Ext_ArtPollReply *reply = (T_Ext_ArtPollReply*)buffer;
+    memset(buffer, 0, sizeof(T_Ext_ArtPollReply) + sizeof(short)*config->numPorts);
+    
+    memcpy(reply->ID, "Art-Ext", 8) ;
+    reply->OpCode = OpPollReply | 0x0001;
+    memcpy(reply->BoxAddr, config->ip, 4);
+    
+    memcpy(reply->Mac, config->mac, 6);
+
+    reply->ProtocolVersionLo = 1;
+    reply->ProtocolVersionHi = 0;
+    
+
+    reply->ResponseNum = 1;
+    reply->TotalResponses = 1;
+
+    reply->NumPortsIn = 0;
+    reply->NumPortsOut = config->numPorts;
+
+    uchar * addresses = (uchar*)(buffer + sizeof(T_Ext_ArtPollReply));
+    for(int i=0;i<config->numPorts;i++){
+      addresses[i*2] =0;  
+            addresses[i*2+1] =i;  
+    }
+    
+  /*  
+   *   
+    reply->VersionInfoHi = config->verHi;
+    reply->VersionInfoLo = config->verLo;
+    
+    reply->NetSwitch = config->net;
+    reply->SubSwitch = config->subnet;
+
+    strcpy((char*)reply->ShortName, config->shortName);
+    strcpy((char*)reply->LongName, config->longName);
+    
+    reply->NumPortsLo = config->numPorts;
+    memcpy(reply->PortTypes, config->portTypes, 4);
+    memset(reply->GoodOutput, 0x80, config->numPorts); // Very important for MadMapper!
+    memcpy(reply->SwIn, config->portAddrIn, 4);
+    memcpy(reply->SwOut, config->portAddrOut, 4);
+    reply->Style = StyleNode;
+    */
+}
+
+void ArtNodeExtended::createPollReply() {
+    ArtPollReply *reply = (ArtPollReply*)buffer;
+    memset(buffer, 0, sizeof(ArtPollReply));
+    
+    memcpy(reply->ID, "Art-Net", 8);
+    reply->OpCode = OpPollReply;
+    memcpy(reply->BoxAddr.IP, config->ip, 4);
+    reply->BoxAddr.Port = config->udpPort;
+    
+    reply->VersionInfoHi = config->verHi;
+    reply->VersionInfoLo = config->verLo;
+    
+    reply->NetSwitch = config->net;
+    reply->SubSwitch = config->subnet;
+
+    strcpy((char*)reply->ShortName, config->shortName);
+    strcpy((char*)reply->LongName, config->longName);
+
+    int numPorts = config->numPorts;
+    if(numPorts > 4) numPorts = 4;
+    reply->NumPortsLo = numPorts;
+    memcpy(reply->PortTypes, config->portTypes, 4);
+    memset(reply->GoodOutput, 0x80, numPorts); // Very important for MadMapper!
+    memcpy(reply->SwIn, config->portAddrIn, 4);
+    memcpy(reply->SwOut, config->portAddrOut, 4);
+    reply->Style = StyleNode;
+    memcpy(reply->Mac, config->mac, 6);
+}
+
+
---
+++ ArtNetFrameExtension.h
@@ -0,0 +1,47 @@
+/**
+* ArtNet Frame Extenstion understood by MadMapper
+**/
+
+#include "ArtNode.h"
+
+typedef struct S_Ext_ArtPoll {
+    uchar ID[8];                    // protocol ID = "Art-Ext"
+    ushort OpCode;                  // == OpPoll + 1 0x2001
+    uchar ProtVerHi;                // 0
+    uchar ProtVerLo;                // protocol version, set to ProtocolVersion
+} T_Ext_ArtPoll;
+
+typedef struct S_Ext_ArtPollReply {
+  uchar ID[8];                    // protocol ID = "Art-Ext"
+  ushort OpCode;                  // == OpPollReply + 1 
+  
+  uchar ProtocolVersionHi;        // The protocol version of ART-EXT (1 currently)
+  uchar ProtocolVersionLo;        // The protocol version of ART-EXT (1 currently)
+  
+  uchar BoxAddr[4];                 // 0 if not yet configured
+  uchar Mac[6];                   // Mac Address, zero if info not available
+
+  uchar ResponseNum;              // This is packet x of y
+  uchar TotalResponses;           // There are y packets
+
+  uchar NumPortsIn;
+  uchar NumPortsOut;
+
+  //short swinout; 
+  
+
+} T_Ext_ArtPollReply;
+
+
+
+class ArtNodeExtended : public ArtNode {
+public:
+  ArtNodeExtended();
+  ArtNodeExtended(ArtConfig & config, int size, unsigned char * buffer);
+
+  void createPollReply();
+  void createExtendedPollReply();
+  int sizeOfExtendedPollReply();
+
+};
+
